613f8aa4e48d7753070abee21d6da35dde0bc4d2
pierre rubens milorme <pierrerubens.milorme@hopitalweb.ht>
2022-03-18 16:14:19 -0400

    creating state folder and files for auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,15 +2,19 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthReducer } from "./state/auth.reducer";
+import { AUTH_STATE_NAME } from "./state/auth.selectors";
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: '', redirectTo: 'login' },
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
     ]
   }
 ];
@@ -23,6 +27,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    StoreModule.forFeature(AUTH_STATE_NAME, AuthReducer),
     RouterModule.forChild(routes)
   ]
 })
